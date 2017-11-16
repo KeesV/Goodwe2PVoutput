c6a43f6792a7a8855fc0a180440dc5562cba9c29
Kees Verhaar <kverhaar@gmail.com>
2017-11-16 09:43:37 +0100

    Get certain vars from environment to facilitate immutable docker image

--- goodweConfig.py
+++ goodweConfig.py
@@ -44,6 +44,18 @@ class goodweConfig :
              self.spline_fit = line.replace(self.SPLINE_FIT, '').strip() == "True"
 	    if self.INPUT_SOURCE in line:
              self.input_source = line.replace(self.INPUT_SOURCE, '').strip()
+	    
+	    # If there are things defined in the environment variables, those overwrite what is in the config file
+	    if "GOODWE_SYSTEM_ID" in os.environ
+	        self.goodwe_system_id = os.getenv("GOODWE_SYSTEM_ID")
+	    if "GOODWE_USER_ID" in os.environ
+	        self.goodwe_user_id = os.getenv("GOODWE_USER_ID")
+	    if "GOODWE_PASSWORD" in os.environ
+	        self.goodwe_password = os.getenv("GOODWE_PASSWORD")
+	    if "PVOUTPUT_SYSTEM_ID" in os.environ
+	        self.pvoutput_system_id = os.getenv("PVOUTPUT_SYSTEM_ID")
+	    if "PVOUTPUT_API" in os.environ
+	        self.pvoutput_api = os.getenv("PVOUTPUT_API")
 
    #--------------------------------------------------------------------------
    def to_string( self):
